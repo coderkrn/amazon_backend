0e988a01c86d2b1c4fa0f1c8653762f5316354ad
Karan <karanpal03040@gmail.com>
2023-06-18 21:47:06 +0530

    change url in cors

--- app.js
+++ app.js
@@ -12,7 +12,7 @@ const cookieParser = require('cookie-parser')
 const DefaultData = require('./defaultData')
 
 const port = 8005;
-app.use(cors())
+app.use(cors({credentials: true, origin: 'https://amazonbackend-zeta.vercel.app'}))
 
 
 app.use(express.json())
